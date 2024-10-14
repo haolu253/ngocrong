e1e28e8fd3c231c0d0008217628ad146323c495a
haolu253 <luchihao253@gmail.com>
2024-10-14 18:04:09 +0700

    update header

--- js/main.js
+++ js/main.js
@@ -1,2 +1,37 @@
 $(document).ready(function () {
+    $('.hamburger').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active-menu');
+        $('.side-menu').toggleClass('side-menu-active');
+    });
+
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest('.hamburger, .side-menu').length) {
+            $('.hamburger').removeClass('active-menu');
+            $('.side-menu').removeClass('side-menu-active');
+        }
+    });
+
+    var resizeTimeout; // Declare resizeTimeout variable
+
+    function updateHeaderDimensions() {
+        // Get header logo width on desktop
+        var headerLeftWidth = $('.header-left').outerWidth();
+        $(':root').css('--header-logo-width', headerLeftWidth + 'px');
+
+        // Get header height
+        var headerHeight = $('header').outerHeight();
+        $(':root').css('--header-height', headerHeight + 'px');
+    }
+
+    // Initial call to set the values
+    updateHeaderDimensions();
+
+    // Update on window resize with debounce
+    $(window).on('resize', function () {
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(function () { // Set the timeout
+            updateHeaderDimensions();
+        }, 300);
+    });
 });
