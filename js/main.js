34bd0cca05e9043bca6eaa854ad21f1819e2f85d
haolu253 <luchihao253@gmail.com>
2024-11-07 14:17:23 +0700

    header complete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 $(document).ready(function () {
+    $('.hamburger').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active-menu');
+        $('.side-menu').toggleClass('side-menu-active');
+        $('body').toggleClass('no-scroll');
+    });
 });
