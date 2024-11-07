061ace75c87ce7a6e330de593e197be930d33bc2
haolu253 <luchihao253@gmail.com>
2024-11-07 14:38:49 +0700

    go top button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
+    // Hamburger mobile
     $('.hamburger').on('click', function (e) {
         e.preventDefault();
         $(this).toggleClass('active-menu');
         $('.side-menu').toggleClass('side-menu-active');
         $('body').toggleClass('no-scroll');
+    });
+
+    // Floating to top
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('.floating-btn').addClass('visible');
+        } else {
+            $('.floating-btn').removeClass('visible');
+        }
+    });
+
+    $('.go-top-btn').on('click', function (e) {
+        e.preventDefault(); // Prevent any default behavior
+        $('html, body').scrollTop(0);
     });
 });
