9f47d6dbd29ff4179bfdd0a637641057ad8eb81e
haolu253 <luchihao253@gmail.com>
2024-11-11 16:13:34 +0700

    home

--- js/main.js
+++ js/main.js
@@ -27,9 +27,31 @@ $(document).ready(function () {
         $('html, body').scrollTop(0);
     });
 
+    // Function to initialize Slick carousels
+    const initializeSlick = (slickContainerId) => {
+        if (!$(slickContainerId).find('.planet-character-information').hasClass('slick-initialized')) {
+            // Initialize Slick for planet-character-information
+            $(slickContainerId + ' .planet-character-information').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                fade: true,
+            });
+        }
+    };
+
+    // Function to check screen width and initialize Slick if needed
+    const initSlickOnResize = () => {
+        if ($(window).width() < 992) {
+            $('.planet-slick').each(function () {
+                const slickContainerId = `#${$(this).attr('id')}`;
+                initializeSlick(slickContainerId);
+            });
+        }
+    };
+
     // Click event for selecting a planet
     $('.planet').on('click', function () {
-        // Get the ID of the selected planet
         const selectedPlanetId = $(this).attr('id');
         const slickContainerId = `#${selectedPlanetId}-slick`;
 
@@ -43,30 +65,13 @@ $(document).ready(function () {
         // Add "planet-slick-active" to the selected slick container
         $(slickContainerId).addClass('planet-slick-active');
 
-        // Check if the selected planet's slider is already initialized
-        if (!$(slickContainerId).find('.planet-character-information').hasClass('slick-initialized')) {
-            // Initialize Slick for planet-character-information
-            $(slickContainerId + ' .planet-character-information').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-                fade: true,
-                asNavFor: slickContainerId + ' .planet-character-thumb'
-            });
-
-            // Initialize Slick for planet-character-thumb
-            $(slickContainerId + ' .planet-character-thumb').slick({
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                asNavFor: slickContainerId + ' .planet-character-information',
-                dots: true,
-                centerMode: true,
-                focusOnSelect: true
-            });
+        // Initialize Slick only if the screen width is smaller than 992px
+        if ($(window).width() < 992) {
+            initializeSlick(slickContainerId);
         }
 
+        // Reset the active slide to the first slide (index 0)
         $(slickContainerId + ' .planet-character-information').slick('slickGoTo', 0);
-        $(slickContainerId + ' .planet-character-thumb').slick('slickGoTo', 0);
     });
 
     // Initialize the default active planet (Earth)
@@ -74,21 +79,13 @@ $(document).ready(function () {
     $('.planet-slick').removeClass('planet-slick-active'); // Remove all active classes initially
     $('#earth-slick').addClass('planet-slick-active');     // Add active class to Earth slick by default
 
-    // Initialize Earth Slick by default
-    $('#earth-slick .planet-character-information').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '#earth-slick .planet-character-thumb'
-    });
+    // Initialize Slick for Earth by default if the screen width is smaller than 992px
+    if ($(window).width() < 992) {
+        initializeSlick('#earth-slick');
+    }
 
-    $('#earth-slick .planet-character-thumb').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '#earth-slick .planet-character-information',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true
+    // Check for window resize to initialize Slick if needed
+    $(window).on('resize', function () {
+        initSlickOnResize();
     });
 });
